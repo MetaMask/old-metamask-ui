9bd621c8deaaf767370dcf61d9dd59319138a504
Dan Finlay <dan@danfinlay.com>
2016-03-04 15:48:24 -0800

    Add rudimentary rpc setting UI

--- app/config.js
+++ app/config.js
@@ -37,10 +37,51 @@ ConfigScreen.prototype.render = function() {
 
       // conf view
       h('.flex-column.flex-justify-center.flex-grow.select-none', [
+        h('.flex-space-around', {
+          style: {
+            padding: '20px',
+          }
+        }, [
 
-        h('h3', 'Current RPC:'),
-        h('p', rpc)
+          h('div', [
+            h('h3', {style: { 'font-weight': 'bold' }}, 'Current RPC'),
+            h('p', rpc)
+          ]),
 
+          h('div', [
+            h('input', {
+              placeholder: 'New RPC URL',
+              style: {
+                width: '100%',
+              },
+              onKeyPress(event) {
+                if (event.key === 'Enter') {
+                  var element = event.target
+                  var newRpc = element.value
+                  state.dispatch({
+                    type: actions.SET_RPC_TARGET,
+                    value: newRpc,
+                  })
+                }
+              }
+            }),
+          ]),
+
+          h('div', [
+            h('button', {
+              style: {
+                'align-self': 'center',
+              },
+              onClick(event) {
+                event.preventDefault()
+                state.dispatch({
+                  type: actions.SET_RPC_TARGET,
+                  value: 'https://rawtestrpc.metamask.io/',
+                })
+              }
+            }, 'Use Default (Test Network)')
+          ])
+        ]),
       ]),
     ])
   )
